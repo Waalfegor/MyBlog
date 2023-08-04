6f50adeba8171fd10fa9cdd42560436ce34f7f34
Waalfegor <fomin.egor03@gmai.com>
2023-08-04 23:03:26 +0300

    Initial commit

---
+++ demo/src/main/java/com/example/demo/controllers/BlogController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/blog")
+    public String blogMain(String name, Model model){
+        model.addAttribute("blog","Блог");
+        return "blog-main";
+    }
+}
---
+++ demo/src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home( String name, Model model) {
+        model.addAttribute("title","Главная страничка");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about( String name, Model model) {
+        model.addAttribute("about","Обо мне");
+        return "about";
+    }
+}
+
+
+
