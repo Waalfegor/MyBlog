da227301e75d7f4e08548596fda4d807fb1f9dab
Waalfegor <100232625+Waalfegor@users.noreply.github.com>
2023-08-15 18:56:21 +0300

    Added function to edit posts

--- demo/src/main/java/com/example/demo/controllers/BlogController.java
+++ demo/src/main/java/com/example/demo/controllers/BlogController.java
@@ -51,4 +51,27 @@ public class BlogController {
         model.addAttribute("post", result);
         return "blog-details";
     }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable("id") Long id, Model model){
+        if (!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable("id") Long id, @RequestParam String title, @RequestParam String anons,
+                              @RequestParam String full_text){
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
 }
